72b5e6db9ee58b73269b1adc85310dece1d406ed
rsj <rseanjustice@gmail.com>
2024-04-06 20:52:24 -0700

    adding service unit tests

--- internal/service/reddit_test.go
+++ internal/service/reddit_test.go
@@ -91,3 +91,73 @@ func TestReddit_GetSubredditMostUps(t *testing.T) {
 		})
 	}
 }
+
+func TestReddit_GetSubredditAuthorPosts(t *testing.T) {
+	type fields struct {
+		Manager func(ctrl *gomock.Controller) Manager
+	}
+	type args struct {
+		req *redditv1.GetSubredditAuthorPostsRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *redditv1.GetSubredditAuthorPostsResponse
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Manager: func(ctrl *gomock.Controller) Manager {
+					m := NewMockManager(ctrl)
+					r := []*redditv1.SubredditPost{
+						{
+							Author:         "dbzcat",
+							AuthorFullname: "aabbgg",
+							Posts:          34,
+						},
+					}
+					m.EXPECT().SubredditAuthorPosts(gomock.Any(), "funny", 1).Return(r, nil)
+					return m
+				},
+			},
+			args: args{
+				req: &redditv1.GetSubredditAuthorPostsRequest{
+					Subreddit: "funny",
+					Limit:     1,
+				},
+			},
+			want: &redditv1.GetSubredditAuthorPostsResponse{
+				AuthorPosts: []*redditv1.SubredditPost{
+					{
+						Author:         "dbzcat",
+						AuthorFullname: "aabbgg",
+						Posts:          34,
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			r := &Reddit{
+				Manager: tt.fields.Manager(ctrl),
+			}
+			got, err := r.GetSubredditAuthorPosts(context.Background(), tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Reddit.GetSubredditAuthorPosts() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			opts := cmp.Options{
+				protocmp.Transform(),
+			}
+			if !cmp.Equal(got, tt.want, opts) {
+				t.Errorf("Reddit.GetSubredditMostUps() (-want, +got) %s", cmp.Diff(tt.want, got, opts))
+			}
+		})
+	}
+}
